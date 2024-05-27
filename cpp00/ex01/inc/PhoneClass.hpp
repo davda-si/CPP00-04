14ccd29b6a9d0fa3771f12d02ba2dd3d5c663229
david <davda-si@student.42lisboa.com>
2024-05-27 15:10:20 +0100

    learning sum stuff

--- cpp00/ex01/inc/PhoneClass.hpp
+++ cpp00/ex01/inc/PhoneClass.hpp
@@ -1,1 +1,14 @@
 #include <iostream>
+
+class PhoneClass
+{
+	private:
+		int myVariable;
+		float myFloat;
+
+	public:
+		PhoneClass(int value, float floatValue) : myVariable(value), myFloat(floatValue)
+		{
+
+		}
+};
